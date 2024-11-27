ef92aaff32a50d926cb798bb0cf06a136958a367
a0937370704@gmail.com <a0937370704@gmail.com>
2024-11-27 16:35:18 +0800

    CI

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,7 +8,11 @@
     </div>
     <div class="q-pl-lg q-mb-lg"><router-link to="/faq">faq</router-link></div>
     <div class="q-pl-lg q-mb-lg">
-      <a href="/products">products</a>
+      <a
+        target="_blank"
+        href="https://lgtits.github.io/test404/products/products.html"
+        >products</a
+      >
     </div>
   </div>
 
