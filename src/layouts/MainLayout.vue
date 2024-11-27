1d726820411c8e2618d8509836b0e1737b2bc847
a0937370704@gmail.com <a0937370704@gmail.com>
2024-11-27 15:54:30 +0800

    init project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="q-mt-lg">
+    <div class="q-pl-lg q-mb-lg">
+      <router-link to="/">index</router-link>
+    </div>
+    <div class="q-pl-lg q-mb-lg">
+      <router-link to="/demo">demo</router-link>
+    </div>
+    <div class="q-pl-lg q-mb-lg"><router-link to="/faq">faq</router-link></div>
+    <div class="q-pl-lg q-mb-lg">
+      <a href="/products">products</a>
+    </div>
+  </div>
+
+  <router-view></router-view>
+</template>
+
+<script setup></script>
---
+++ src/router/routes.js
@@ -0,0 +1,20 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      { path: "/demo", component: () => import("pages/DemoPage.vue") },
+      { path: "/faq", component: () => import("pages/FaqPage.vue") },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
+
+export default routes;
