b859b8ded2ede22c53243a7e5236632a69c23b8c
a0937370704@gmail.com <a0937370704@gmail.com>
2024-11-27 15:49:55 +0800

    init project

---
+++ dist/spa/assets/IndexPage.024da79f.js
@@ -0,0 +1,1 @@
+import{_ as s,e as a,o as t,c as o,a as r}from"./index.a02a3912.js";var n="/assets/quasar-logo-vertical.55e9c854.svg";const c=a({name:"IndexPage"}),l={class:"flex flex-center"};function i(p,e,d,_,f,x){return t(),o("div",l,e[0]||(e[0]=[r("img",{alt:"Quasar logo",src:n,style:{width:"200px",height:"200px"}},null,-1)]))}var g=s(c,[["render",i]]);export{g as default};
